3eebca3df79334a2bda977888ff114f7d4fed31c
Bluefire2 <chernyshov.k@gmail.com>
2017-12-25 12:37:43 +0000

    Implement bubble sort

---
+++ src/org/bluefire2/Sorts/BubbleSort.java
@@ -0,0 +1,28 @@
+package org.bluefire2.Sorts;
+
+import org.bluefire2.Operation;
+
+import java.util.ArrayList;
+
+public class BubbleSort extends Sort {
+    @Override
+    public ArrayList<Operation> run(int[] data) {
+        final ArrayList<Operation> ops = new ArrayList<>();
+        int[] dataCopy = new int[data.length];
+        System.arraycopy(data, 0, dataCopy, 0, data.length);
+
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for(int i = 1; i < dataCopy.length; i++) {
+                if(dataCopy[i - 1] > dataCopy[i]) {
+                    swap(dataCopy, i - 1, i, ops);
+                    swapped = true;
+                }
+            }
+        } while(swapped); // stop when no elements need swapping (i.e. array is sorted)
+
+        return ops;
+    }
+}
