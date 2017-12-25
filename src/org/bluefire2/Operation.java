e5a30c021ee1906ea3857ef8414e4f396d7ce06f
Bluefire2 <chernyshov.k@gmail.com>
2017-12-25 12:25:22 +0000

    Change Operation documentation
    Operations now directly modify the input data

--- src/org/bluefire2/Operation.java
+++ src/org/bluefire2/Operation.java
@@ -2,8 +2,7 @@ package org.bluefire2;
 
 public interface Operation {
     /**
-     * This method applies the operation to an array. The input array is *not* modified; rather, a modified copy is
-     * returned.
+     * This method applies the operation to an array. The input data is modified.
      *
      * @param data The array to apply the operation to. If the operation cannot be applied to the input, e.g. the
      *             operation needs to access index 10 but the input only has 10 elements, then, an
