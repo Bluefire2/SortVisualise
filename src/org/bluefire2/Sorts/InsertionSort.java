d29fa2267614cb321756c1f7e67c334d34b3a6ea
Bluefire2 <chernyshov.k@gmail.com>
2017-12-25 14:25:26 +0000

    Implement insertion sort

---
+++ src/org/bluefire2/Sorts/InsertionSort.java
@@ -0,0 +1,28 @@
+package org.bluefire2.Sorts;
+
+import org.bluefire2.Operation;
+import org.bluefire2.Sort;
+
+import java.util.ArrayList;
+
+public class InsertionSort extends Sort {
+
+    @Override
+    public ArrayList<Operation> run(int[] data) {
+        final ArrayList<Operation> ops = new ArrayList<>();
+        int[] dataCopy = new int[data.length];
+        System.arraycopy(data, 0, dataCopy, 0, data.length);
+
+        for(int i = 1; i < dataCopy.length; i++) {
+            for(int j = i; j > 0; j--) {
+                if(dataCopy[j - 1] < dataCopy[j]) {
+                    break;
+                } else {
+                    swap(dataCopy, j - 1, j, ops);
+                }
+            }
+        }
+
+        return ops;
+    }
+}
