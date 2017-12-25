1efa20d64d4682bec7c80a5615e719d43a4c2b5b
Bluefire2 <chernyshov.k@gmail.com>
2017-12-25 12:46:48 +0000

    Add simple test case to Main

--- src/org/bluefire2/Main.java
+++ src/org/bluefire2/Main.java
@@ -1,8 +1,18 @@
 package org.bluefire2;
 
+import org.bluefire2.Sorts.BubbleSort;
+
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InvalidOperationException {
+        int[] data = new int[] {5, 4, 3, 2, 1};
+        Sort bs = new BubbleSort();
+        ArrayList<Operation> ops = bs.run(data);
+        for(Operation op : ops) {
+            System.out.println(op);
+            op.apply(data);
+        }
     }
 }
