cd536136d87390e1eb81a2da08e98b09b6619c38
Bluefire2 <chernyshov.k@gmail.com>
2017-12-25 12:26:46 +0000

    Add Sort base class

---
+++ src/org/bluefire2/Sorts/Sort.java
@@ -0,0 +1,18 @@
+package org.bluefire2.Sorts;
+
+import org.bluefire2.Operation;
+import org.bluefire2.Swap;
+import java.util.ArrayList;
+
+public abstract class Sort {
+    // some helper methods
+    private void swap(int[] data, int a, int b, ArrayList<Operation> ops) {
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
+
+        ops.add(new Swap(a, b));
+    }
+
+    public abstract ArrayList<Operation> run(int[] data);
+}
