0d85660d4de251aeededa0426b8fb00fff0b85e3
Bluefire2 <chernyshov.k@gmail.com>
2017-12-27 18:19:38 +0200

    Implement operation type logging

--- src/org/bluefire2/UI/ArrayGraphRenderer.java
+++ src/org/bluefire2/UI/ArrayGraphRenderer.java
@@ -1,7 +1,10 @@
 package org.bluefire2.UI;
 
 import org.bluefire2.InvalidOperationException;
+import org.bluefire2.Operations.Comparison;
+import org.bluefire2.Operations.Lookup;
 import org.bluefire2.Operations.Operation;
+import org.bluefire2.Operations.Swap;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,12 +16,18 @@ public class ArrayGraphRenderer extends JComponent implements ActionListener {
     private Queue<Operation> ops = null;
     private int[] data = null;
     private Timer timer = null;
+    private int swaps = 0;
+    private int lookups = 0;
+    private int comparisons = 0;
+    private int totalOperations = 0;
 
     public void init(int delay, int[] data, Queue<Operation> ops) {
         this.data = data;
         this.ops = ops;
-        this.timer = new Timer(delay, this);
-        this.timer.start();
+        timer = new Timer(delay, this);
+        timer.start();
+
+        swaps = lookups = comparisons = 0;
     }
 
     @Override
@@ -34,7 +43,17 @@ public class ArrayGraphRenderer extends JComponent implements ActionListener {
             try {
                 Operation current = ops.remove();
                 current.apply(data);
-                // this.ops.remove().apply(this.data);
+
+                // log the type of operation
+                if(current instanceof Swap) {
+                    swaps++;
+                } else if(current instanceof Lookup) {
+                    lookups++;
+                } else if(current instanceof Comparison) {
+                    comparisons++;
+                }
+                totalOperations++;
+
             } catch(InvalidOperationException e) {
                 e.printStackTrace();
             }
